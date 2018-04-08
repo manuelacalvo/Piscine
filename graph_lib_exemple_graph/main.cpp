805cbd65978a3ad67ecf4883998e7ecc4a440de0
manuelacalvo <manuela.calvo@edu.ece.fr>
2018-04-08 11:13:40 +0200

    graphe reduit

--- graph_lib_exemple_graph/main.cpp
+++ graph_lib_exemple_graph/main.cpp
@@ -15,27 +15,21 @@ int main()
     /// Un exemple de graphe
     Graph g;
     ///choix du monde
+    int choix1=0;
     int sauv = 0;
     ///choix new ou sauvé
     int choix2=0;
-    int world;
-    while(g.GetMonde() != 4 || (g.GetMonde()!=1 && g.GetMonde() !=2 && g.GetMonde()!=3) )
+    while(choix1 != 4)
 
     {
-
          std::cout<<" choississez le reseau que vous souhaitez ou quitter"<<std::endl<<"1. la banquise  2.la savane  3.la foret  4.quitter"<<std::endl;
-    std::cin>>world;
-    g.SetMonde(world);
-if(g.GetMonde()==1 || g.GetMonde() ==2 || g.GetMonde()==3 )
-{
-
-
+    std::cin>>choix1;
 
 std::cout<<" 1. nouveau graphe  2.chargement graphe sauvgarde "<<std::endl;
 std::cin>>choix2;
 
 
-     g.editer_reseaux(choix2);
+     g.editer_reseaux(choix1,choix2);
 
     //g.colorier_compo_connexe();
 
@@ -47,84 +41,56 @@ std::cin>>choix2;
 //g.supp_arete(9);
 
 
+
+sauv=0;
     /// Vous gardez la main sur la "boucle de jeu"
     /// ( contrairement à des frameworks plus avancés )
-while (g.get_interface()->m_menu.get_value()==false)
+while ( !key[KEY_ESC])
     {
         /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
        g.update();
 
-       ///appel sauvegarde
         if(g.get_interface()->m_sauvegarde.get_value() == true)
-        {
+        {   sauv++;
+
+
             g.sauvegarde();
         }
 
-        ///appel connexite
+
         if(g.get_interface()->m_connexite.get_value() == true)
         {
              g.colorier_compo_connexe();
-
-
         }
-
-        ///appel dynamique de population
         if(g.get_interface()->m_animation.get_value() == true)
         {
              g.dynamique_pop1();
         }
-
-       ///appel ajouter sommet
-         if(g.get_interface()->m_ajout_s.get_value() == true)
-         {
-             std::cout<<" dans le if "<<std::endl;
-             g.ajout_sommet();
-
-
-         }
-
-         ///appel ajouter arete
-         if(g.get_interface()->m_ajout_a.get_value()==true)
-         {
-             g.ajout_arete_selec();
-         }
-
-         ///appel fortement connexe
-        if(g.get_interface()->m_fort_connexite.get_value()==true)
+        if(key[KEY_DOWN])
         {
-            g.get_interface()->m_fort_connexite.set_value(false);
            g.fortement_connexe();
         }
-
-       ///appel suppresion sommet
-        g.supp_sommet_selec();
-
-        ///appel k_connexité
-        if(g.get_interface()->m_k_connexite.get_value()== true)
+       g.supp_sommet_selec();
+        if(key[KEY_K])
         {
-            g.get_interface()->m_k_connexite.set_value(false);
             g.changement_nb_desactive();
 
         }
 
 
 
-    //std::cout<<" avant supp arete "<<std::endl;
-        ///appel suppression arete
-       g.supp_arete_test();
 
+
+
+       g.supp_arete_test();
 
         /// Mise à jour générale (clavier/souris/buffer etc...)
         grman::mettre_a_jour();
 
 
     }
-    }
-    ///blindage saisie
-       else
-       std::cout<<" choississez parmis l'un des trois mondes proposes svp ... "<<std::endl;
-if(g.get_interface()->m_menu.get_value()==false)
-        g.get_interface()->m_menu.set_value(true);
+
+
     }
 
     grman::fermer_allegro();
